aab95c2d4639a833a3269b641a07392b55c2b19f
BWFF <william.f89@gmail.com>
2017-11-07 09:48:02 -0200

    Commit local

---
+++ PostGetModel/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using PostGetModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostGetModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa()
+            {
+                PessoaId = 1,
+                Nome = "Bruno William",
+                Twitter = "@BrunoWilliam"
+            };
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+    }
+}
---
+++ PostGetModel/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PostGetModel.Models
+{
+    public class Pessoa
+    {
+        [Display(Name = "Código")]
+        public int PessoaId { get; set; }
+
+        [Required(ErrorMessage="Informe o nome.!")]
+        public string Nome { get; set; }
+        public string Twitter { get; set; }
+    }
+}
